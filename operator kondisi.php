5bd44318c54ce529da0455f6a41abac29c3579b6
NayaRPL <naya.mmj@gmail.com>
2022-02-15 22:11:47 +0800

    day 66

---
+++ operator kondisi.php
@@ -0,0 +1,35 @@
+<?php 
+// operator IF
+$nilai = 89;
+if ($nilai >= 75) {
+  echo "selamat, anda lulus ujian" ;
+}
+?>
+
+<?PHP
+// operator IF ELSE
+$nilai = 55;
+if($nilai >= 75){
+    echo " selamat, anda lulus ujian";
+}
+else {
+    echo " mohon maaf anda tidak lulus ujian";
+}
+?>
+
+<?php
+// operator if else if
+$nilai=70;
+if ($nilai>=85){
+    echo"Nilai anda : A ";
+}
+else if ($nilai>=75){
+    echo " Nilai anda : B"; 
+}
+else if ($nilai >= 55){
+    echo " Nilai anda : C";
+}
+else {
+    echo " Nilai anda : D";
+}
+?>
